569621b3785355afe2d9223bfe9753ed4cca480f
huy <47073353+mykubernetes@users.noreply.github.com>
2019-03-24 15:37:40 +0800

    Create my-k8s-job-yaml.groovy

---
+++ CICD/jenkins/my-k8s-job-yaml.groovy
@@ -0,0 +1,47 @@
+podTemplate(label: "kubernetes", yaml: """
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+  - name: kubectl
+    image: aishangwei/kubectl
+    command: ["sleep"]
+    args: ["100000"]
+  - name: oc
+    image: aishangwei/openshift-client
+    command: ["sleep"]
+    args: ["100000"]
+  - name: golang
+    image: golang:1.9
+    command: ["sleep"]
+    args: ["100000"]
+  - name: helm
+    image: aishangwei/helm:2.8.2
+    command: ["sleep"]
+    args: ["100000"]
+"""
+) {
+    node("kubernetes") {
+        container("kubectl") {
+            stage("kubectl") {
+                sh "kubectl version"
+            }
+        }
+        container("oc") {
+            stage("oc") {
+                sh "oc version"
+            }
+        }
+        container("golang") {
+            stage("golang") {
+                sh "go version"
+            }
+        }
+        container("helm") {
+            stage("helm") {
+                sh "helm version"
+            }
+        }
+    }
+}
+
